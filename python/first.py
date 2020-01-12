34c64ce220661307b5b7d7cb721c3882922d4a73
yjnwjs <jnyuan@xmut.edu.cn>
2020-01-12 16:39:08 +0800

    commit first.py

---
+++ python/first.py
@@ -0,0 +1,13 @@
+# -*- coding:utf-8 -*-
+# Filename: rwexcel_scoretransfer.py
+# --------------------
+# Function description:
+# 读取Excel成绩文件，并且将5级制成绩转换成百分制
+# Author: John Yuan
+# QQ: 306467355
+# Email: jnyuan@xmut.edu.cn
+#--------------------
+# Date: 2018-12-21
+# --------------------
+import os
+import sys
